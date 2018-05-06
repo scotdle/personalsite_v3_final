977500896ede426cd7009c086389bbdb5310ac12
Scott Caudle <scottcaudlewebsite@gmail.com>
2018-05-05 21:01:37 -0400

    added some JS that displays users name when the name form input is filled out.

--- page-templates/contactpagetemplate.php
+++ page-templates/contactpagetemplate.php
@@ -12,31 +12,53 @@ get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-	<div class="wrapper" id="full-width-page-wrapper">
+<div class="wrapper" id="full-width-page-wrapper">
 
-		<div class="<?php echo esc_attr( $container ); ?> " id="content">
+    <div class="<?php echo esc_attr( $container ); ?> " id="content">
 
-			<div class="row">
+        <div class="row">
 
-				<div class="col-md-12 content-area" id="primary">
+            <div class="col-md-12 content-area" id="primary">
 
-					<main class="site-main" id="main" role="main">
+                <main class="site-main" id="main" role="main">
 
-						<?php while ( have_posts() ) : the_post(); ?>
+					<?php while ( have_posts() ) : the_post(); ?>
 
-							<?php get_template_part( 'loop-templates/content-contact', 'page' ); ?>
+						<?php get_template_part( 'loop-templates/content-contact', 'page' ); ?>
 
-						<?php endwhile; // end of the loop. ?>
+					<?php endwhile; // end of the loop. ?>
 
 
-					</main><!-- #main -->
+                </main><!-- #main -->
 
-				</div><!-- #primary -->
+            </div><!-- #primary -->
 
-			</div><!-- .row end -->
+        </div><!-- .row end -->
 
-		</div><!-- Container end -->
+    </div><!-- Container end -->
 
-	</div><!-- Wrapper end -->
+</div><!-- Wrapper end -->
 
+
+<script>
+
+    const namegiven = document.getElementsByClassName("your-name")[0].firstChild;
+
+    namegiven.addEventListener("blur", nameAlert);
+
+    function nameAlert() {
+        entrytitle = document.getElementsByClassName("entry-title")[0];
+
+      const  entrytitlemessage = "nice to meet you " + namegiven.value + "!";
+
+        entrytitle.innerHTML =  entrytitlemessage;
+
+if (namegiven.value === "") {
+
+    entrytitle.innerHTML =  "lets talk";
+
+}
+    }
+</script>
 <?php get_footer(); ?>
+
